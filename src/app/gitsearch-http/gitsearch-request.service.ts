b3fd0f0295a26bdafe2a4ccd53b799303c4736f6
Wecode <mashleyalpha@gmail.com>
2019-02-05 17:07:59 +0200

    creates a user form

--- src/app/gitsearch-http/gitsearch-request.service.ts
+++ src/app/gitsearch-http/gitsearch-request.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
-
+import { Repository } from '../repository';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { User } from '../user';
 @Injectable({
   providedIn: 'root'
 })
-export class GitsearchRequestService {
+export class HttpRequestService {
+  user = new User()
+  repos: Repository;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.repos = new Repository(0, 0, 0, "");
+  }
+
+  reposRequest(user) {
+
+    interface ApiResponse {
+      public_repos: number;
+      following: number;
+      followers: number;
+      public_gists: string;
+
+    }
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<ApiResponse>('https://api.github.com/users/' + this.user.name + '?access_token=' + environment.api_key).toPromise().then(response => {
+
+        this.repos.public_repos = response.public_repos
+        this.repos.following = response.following
+        this.repos.followers = response.followers
+        this.repos.public_gists = response.public_gists
+
+        resolve()
+      },
+        error => {
+          // this.quote.quote="Never, never, never give up."
+          // this.quote.author="winston churchill"
+          reject(error)
+          console.log(error)
+        }
+      )
+    })
+
+    return promise
+  }
 }
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { User } from '../user'
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  newUser = new User("");
+  @Output() add = new EventEmitter<User>();
+  submitForm() {
+    this.add.emit(this.newUser);
+    // this.newUser=new User("");
+  }
 
   constructor() { }
 
