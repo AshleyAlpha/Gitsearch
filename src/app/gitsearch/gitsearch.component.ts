38871ecf5d40b22d0ac05c33c47c20150c962c85
Wecode <mashleyalpha@gmail.com>
2019-02-05 17:03:20 +0200

    added elements in repository.ts

--- src/app/gitsearch/gitsearch.component.ts
+++ src/app/gitsearch/gitsearch.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { HttpClient } from '@angular/common/http';
+import { HttpRequestService } from '../repos-request/http-request.service'
 
 @Component({
   selector: 'app-gitsearch',
   templateUrl: './gitsearch.component.html',
+  providers: [HttpRequestService],
   styleUrls: ['./gitsearch.component.css']
 })
-export class GitsearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class GithubComponent implements OnInit {
+  Users: User
+  addNew(user) {
+    // this.User.push(User)
+    this.Users = new User()
+  }
+  constructor(private http: HttpClient, private reposRequest: HttpRequestService) {
+    this.Users = new User()
   }
 
-}
+  ngOnInit() {
+    // this.httpService.reposRequest()
+    // this.repos=this.httpService.repos
+  }
+
+} 10
--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,3 @@
 export class Repository {
+    constructor(public public_repos: number, public followers: number, public following: number, public public_gists: string) { }
 }
